b77e58c5308ade04d2e60044f8447b79a35ffaa7
hgko1207 <khkkhk1207@gmail.com>
2021-05-05 18:47:11 +0900

    for, range, ...args

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 )
 
 func superAdd(numbers ...int) int {
-	for index, number := range numbers {
-		fmt.Println(index, number)
+	total := 0
+	for _, number := range numbers {
+		total += number
 	}
-	return 1
+	return total
 }
 
 func main() {
-	superAdd(1, 2, 3, 4, 5, 6)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
