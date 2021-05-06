631006cb29f8ad6cdca4958eed31d96b52ebfcff
hgko1207 <khkkhk1207@gmail.com>
2021-05-06 11:05:25 +0900

    update

--- main.go
+++ main.go
@@ -1,14 +1,4 @@
 package main
 
-import "fmt"
-
-func canIDrink(age int) bool {
-	if koreaAge := age + 2; koreaAge < 20 {
-		return false
-	}
-	return true
-}
-
 func main() {
-	fmt.Println((canIDrink(20)))
 }
