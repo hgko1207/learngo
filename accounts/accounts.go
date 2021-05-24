d2140cb5117eae34cc1fe910c8cc922c4a5ae0e6
hgko1207 <khkkhk1207@gmail.com>
2021-05-24 23:27:35 +0900

    UPDATE

---
+++ accounts/accounts.go
@@ -0,0 +1,36 @@
+package accounts
+
+import "errors"
+
+// Account struct
+type Account struct {
+	owner   string
+	balance int
+}
+
+var errNoMoney = errors.New("Can't withdraw you are poor")
+
+// NewAccount creates Account
+func NewAccount(owner string) *Account {
+	account := Account{owner: owner, balance: 0}
+	return &account
+}
+
+// Deposit x amount on your account
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+// Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
+}
--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/hgko1207/learngo/accounts"
+)
+
+
 func main() {
+	account := accounts.NewAccount("hgko")
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
 }
