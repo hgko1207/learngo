41d675dc9a954daa0ac9ede740d393f11eae6e8a
hgko1207 <khkkhk1207@gmail.com>
2021-07-08 01:19:51 +0900

    method

--- main.go
+++ main.go
@@ -9,10 +9,19 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("first")
+	word := "hello"
+	definition := "Greeting"
+	//definition, err := dictionary.Search("first")
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
+	}
+
+	hello, _ := dictionary.Search(word)
+	fmt.Println("found:", word, "definition:", hello)
+
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
 	}
 }
