feed9a8eae613f599d8e4bd40c6cca281040cfa4
hgko1207 <khkkhk1207@gmail.com>
2022-01-05 23:01:52 +0900

    update

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"os"
 	"strings"
 
 	"github.com/hgko1207/learngo/scrapper"
 	"github.com/labstack/echo"
 )
 
+const fileName string = "jobs.csv"
+
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 
 func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
 	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")) )
-	return nil
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
 }
 
 func main() {
